178ac635dee9b534182fb5aa491883fe4f56c5c1
Wojciech Kępka <wojciech@wkepka.dev>
2023-02-16 16:41:36 +0100

    Refactor tarball

--- src/tarball.rs
+++ src/tarball.rs
@@ -4,9 +4,48 @@ use flate2::{write::GzEncoder, Compression};
 use std::{
     fs::{self, File},
     io::{self, Write},
-    path::{Path, PathBuf, MAIN_SEPARATOR},
+    path::{Path, MAIN_SEPARATOR},
 };
 use tar::Builder;
+
+/// Writes a gunzip encoded tarball to `buf` from entries found in `path`.
+pub fn dir<W, P>(buf: W, path: P) -> io::Result<()>
+where
+    W: Write,
+    P: AsRef<Path>,
+{
+    let encoder = GzEncoder::new(buf, Compression::best());
+    let path = path.as_ref();
+    ArchiveBuilder::build(encoder, path)?;
+
+    Ok(())
+}
+
+#[cfg(any(target_os = "linux", target_os = "android", target_os = "freebsd"))]
+#[cfg(feature = "par-compress")]
+/// Same as [`dir`](dir) but initializes the underlying buffer, returns it and utilizes compression
+/// parallelization on multiple cores to speed up the work.
+pub fn dir_par<P>(path: P) -> io::Result<Vec<u8>>
+where
+    P: AsRef<Path>,
+{
+    use gzp::deflate::Gzip;
+    use gzp::par::compress::{ParCompress, ParCompressBuilder};
+    use memfile::MemFile;
+    use std::io::{Read, Seek};
+
+    let tx = MemFile::create_default(&path.as_ref().to_string_lossy())?;
+    let mut rx = tx.try_clone()?;
+    let encoder: ParCompress<Gzip> = ParCompressBuilder::new().from_writer(tx);
+
+    let path = path.as_ref();
+    ArchiveBuilder::build(encoder, path)?;
+
+    rx.rewind()?;
+    let mut data = vec![];
+    rx.read_to_end(&mut data)?;
+    Ok(data)
+}
 
 fn resolve_base_path(canonical_path: &Path) -> io::Result<String> {
     let mut base_path_str = canonical_path
@@ -23,33 +62,25 @@ fn resolve_base_path(canonical_path: &Path) -> io::Result<String> {
 
 struct ArchiveBuilder<W: Write> {
     archive: Builder<W>,
-    canonical: PathBuf,
     base_path: String,
 }
 
 impl<W: Write> ArchiveBuilder<W> {
-    /// Initializes new archive builder that creates a tarball of the `path` directory compressed uzing gzip.
-    pub fn new(archive: Builder<W>, path: &Path) -> io::Result<Self> {
+    fn build(buf: W, path: &Path) -> io::Result<()> {
         let canonical = path.canonicalize()?;
-        let base_path = resolve_base_path(&canonical)?;
+        let mut builder = Self::new(buf, &canonical)?;
+        builder.bundle(&canonical, false)?;
+        builder.archive.finish()?;
+        builder.archive.into_inner()?.flush()
+    }
+
+    fn new(buf: W, canonical: &Path) -> io::Result<Self> {
+        let base_path = resolve_base_path(canonical)?;
 
         Ok(Self {
-            archive,
-            canonical,
+            archive: Builder::new(buf),
             base_path,
         })
-    }
-
-    /// Starts the traversal by bundling files/directories in the base path to the archive.
-    pub fn start(&mut self) -> io::Result<()> {
-        let canonical = self.canonical.clone();
-        self.bundle(&canonical, false)
-    }
-
-    /// Finishes creating the tarball archive.
-    pub fn finish(mut self) -> io::Result<()> {
-        self.archive.finish()?;
-        self.archive.into_inner()?.flush()
     }
 
     /// Starts the traversal by bundling files/directories in the base path to the archive.
@@ -87,48 +118,4 @@ impl<W: Write> ArchiveBuilder<W> {
         }
         Ok(())
     }
-}
-
-/// Writes a gunzip encoded tarball to `buf` from entries found in `path`.
-pub fn dir<W, P>(buf: W, path: P) -> io::Result<()>
-where
-    W: Write,
-    P: AsRef<Path>,
-{
-    let archive = Builder::new(GzEncoder::new(buf, Compression::best()));
-    let path = path.as_ref();
-    let mut builder = ArchiveBuilder::new(archive, path)?;
-    builder.start()?;
-    builder.finish()?;
-
-    Ok(())
-}
-
-#[cfg(any(target_os = "linux", target_os = "android", target_os = "freebsd"))]
-#[cfg(feature = "par-compress")]
-/// Same as [`dir`](dir) but initializes the underlying buffer, returns it and utilizes compression
-/// parallelization on multiple cores to speed up the work.
-pub fn dir_par<P>(path: P) -> io::Result<Vec<u8>>
-where
-    P: AsRef<Path>,
-{
-    use gzp::deflate::Gzip;
-    use gzp::par::compress::{ParCompress, ParCompressBuilder};
-    use memfile::MemFile;
-    use std::io::{Read, Seek};
-
-    let tx = MemFile::create_default(&path.as_ref().to_string_lossy())?;
-    let mut rx = tx.try_clone()?;
-    let pars: ParCompress<Gzip> = ParCompressBuilder::new().from_writer(tx);
-    let archive = Builder::new(pars);
-
-    let path = path.as_ref();
-    let mut builder = ArchiveBuilder::new(archive, path)?;
-    builder.start()?;
-    builder.finish()?;
-
-    rx.rewind()?;
-    let mut data = vec![];
-    rx.read_to_end(&mut data)?;
-    Ok(data)
 }
